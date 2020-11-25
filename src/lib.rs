e0552bc9720b4dc6a04fbd37a4f0a75b50ee1ae2
Owen Nelson <onelson@laika.com>
2020-11-25 17:30:17 +0000

    Refs SGRS-31 Adds overview docs, mention decode via serde and logging HTTP.

--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,130 @@
 //!    library from source as a part of the crate's build script).
 //! - `rustls` (uses the [rustls crate] which is a *pure rust tls implementation*).
 //!
+//! ## Usage
+//!
+//! The general pattern of usage starts with a `shotgun_rs::Shotgun` client.
+//!
+//! ```no_run
+//! # use shotgun_rs::Shotgun;
+//! # #[tokio::main]
+//! # async fn main() -> shotgun_rs::Result<()> {
+//! let server = "https://my-shotgun.example.com";
+//! let script_name = "my-api-user";
+//! let script_key = "********";
+//!
+//! let sg = Shotgun::new(server.to_string(), Some(script_name), Some(script_key))?;
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! Once your client is in hand, you'd use one of the authentication methods to
+//! get an `access_token`.
+//!
+//! ```no_run
+//! # use shotgun_rs::{Shotgun, TokenResponse};
+//! # #[tokio::main]
+//! # async fn main() -> shotgun_rs::Result<()> {
+//! #    let server = "https://my-shotgun.example.com";
+//! #    let script_name = "my-api-user";
+//! #    let script_key = "********";
+//! #    let sg = Shotgun::new(server.to_string(), Some(script_name), Some(script_key))?;
+//! // Authenticates using the script name and script key held by the client.
+//! let TokenResponse { access_token, .. } = sg.authenticate_script().await?;
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! From there, you can pass that access token around to the various query methods.
+//!
+//! For operations where the schema of the response is flexible (based on the
+//! entity type and return fields specified), we use generics to allow the
+//! caller to unpack the response into the type of their choosing. The type just
+//! needs to implement [serde]'s `Deserialize` trait. A number of structs are
+//! provided (ex: `TokenResponse`) to cover responses that are pretty much the
+//! same for everybody.
+//!
+//! Others structs are generic over types deeper in the data structure.
+//! For example, `ResourceArrayResponse<R, L>` is generic over `R`
+//! (the resource) which is the items in the array portion of the response, and
+//! `L` which is the type for the response's "links" key).
+//!
+//! ```no_run
+//! use serde_derive::Deserialize;
+//! use serde_json::json;
+//! use shotgun_rs::types::{PaginationLinks, ResourceArrayResponse, SelfLink};
+//! use shotgun_rs::{Shotgun, TokenResponse};
+//!
+//!
+//! /// This struct should match the return fields specified for the search.
+//! #[derive(Debug, Clone, Deserialize)]
+//! struct ProjectAttrs {
+//!     code: Option<String>,
+//!     name: Option<String>,
+//! }
+//!
+//! #[derive(Clone, Debug, Deserialize)]
+//! struct Project {
+//!     id: Option<i32>,
+//!     r#type: Option<String>,
+//!     attributes: Option<ProjectAttrs>,
+//!     links: Option<SelfLink>,
+//! }
+//!
+//!
+//! #[tokio::main]
+//! async fn main() -> shotgun_rs::Result<()> {
+//!
+//!     let server = "https://my-shotgun.example.com";
+//!     let script_name = "my-api-user";
+//!     let script_key = "********";
+//!
+//!     let sg = Shotgun::new(server.to_string(), Some(script_name), Some(script_key))?;
+//!
+//!     let TokenResponse { access_token, .. } = sg.authenticate_script().await?;
+//!
+//!     let return_fields = ["id", "code", "name"].join(",");
+//!
+//!     // Using type ascription (or a turbofish), we tell search() how to
+//!     // deserialize the response.
+//!     let resp: ResourceArrayResponse<Project, PaginationLinks> = sg
+//!         .search(&access_token, "Project", &return_fields, &json!([]))?
+//!         .size(Some(3))
+//!         .execute()
+//!         .await?;
+//!
+//!     let items = resp.data.unwrap_or_default();
+//!
+//!     for project in items {
+//!         println!("{:?}", project);
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! For times where you don't want to bother defining structs to represent the
+//! response, you can always deserialize to a `serde_json::Value` and interrogate
+//! the value yourself.
+//!
+//! ## Logging
+//!
+//! The `shotgun_rs` crate offers some logging, though most of it relates to the
+//! internals of the library itself.
+//!
+//! If you're interested in logging the HTTP-transport layer, since we're using
+//! [reqwest], you can get some visibility into the transport layer by setting
+//! `reqwest` to `DEBUG`.
+//!
+//! Please refer to the docs for your logger crate to see how to adjust log levels
+//! for crates and modules.
+//!
 //! [native-tls crate]: https://crates.io/crates/native-tls
 //! [rustls crate]: https://crates.io/crates/rustls
 //! [shotgun]: https://www.shotgunsoftware.com/
+//! [reqwest]: https://crates.io/crates/reqwest
+//! [serde]: https://crates.io/crates/serde
+//! [serde_json]: https://crates.io/crates/serde_json
 
 use std::borrow::Cow;
 use std::env;
